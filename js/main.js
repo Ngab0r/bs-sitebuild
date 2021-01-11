5263504262c5f4f9327eea0db3a7cf4a7ab41479
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-01-11 15:46:25 +0100

    clean js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 $(document).on('click', '.nav-link', function (event) {
 
-    // Prevent default events
     event.preventDefault();
 
-    // Animate the body (html page) to scroll to the referring element 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 300);
-
 });
 
 $(window).scroll(function () {
